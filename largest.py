0ec05e717ab76f4d819893a83c7ed76b1ee644cb
Mohanapriya N <41140369+MohanapriyaNadarajan@users.noreply.github.com>
2018-07-12 14:46:13 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,6 +1,10 @@
-lst = []
-num = 3
-for n in range(3):
-    numbers = int(input())
-    lst.append(numbers)
-print( max(lst))
+a=int(input("Enter the number1:"))
+b=int(input("Enter the number2:"))
+c=int(input("Enter the number3:"))
+if(a>b):
+  if(a>c):
+    print(a)
+elif(b>c):
+  print(b)
+else:
+  print(c)
